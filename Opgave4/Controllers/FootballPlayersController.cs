b641829088f9f15250ff3225e76be8c57cfcf1c1
EmilSalomonsen <emil26q4@edu.easj.dk>
2021-10-01 13:06:13 +0200

    Add project files.

---
+++ Opgave4/Controllers/FootballPlayersController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ObligatoriskStudieAktivitetsOpgave;
+using Opgave4.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Opgave4.Controllers
+{
+    [Route("FootballPlayers")]
+    [ApiController]
+    public class FootballPlayersController : ControllerBase
+    {
+        private readonly FootballPlayersManager _manager = new FootballPlayersManager();
+        // GET: api/<FootballPlayersController>
+        [HttpGet]
+        public IEnumerable<FootballPlayer> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<FootballPlayersController>/5
+        [HttpGet("{id}")]
+        public FootballPlayer Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        // POST api/<FootballPlayersController>
+        [HttpPost]
+        public FootballPlayer Post([FromBody] FootballPlayer value)
+        {
+            return _manager.Add(value);
+        }
+
+        // PUT api/<FootballPlayersController>/5
+        [HttpPut("{id}")]
+        public FootballPlayer Put(int id, [FromBody] FootballPlayer value)
+        {
+            return _manager.Update(id, value);
+        }
+
+        // DELETE api/<FootballPlayersController>/5
+        [HttpDelete("{id}")]
+        public FootballPlayer Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ Opgave4/Managers/FootballPlayersManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ObligatoriskStudieAktivitetsOpgave;
+
+namespace Opgave4.Managers
+{
+    public class FootballPlayersManager
+    {
+        private static int _nextId = 1;
+        private static readonly List<FootballPlayer> Data = new List<FootballPlayer>
+        {
+            new FootballPlayer {Id = _nextId++, Name = "Emil", Price = 100, ShirtNumber = 10},
+            new FootballPlayer {Id = _nextId++, Name = "Kasper", Price = 100, ShirtNumber = 11},
+        };
+
+        public List<FootballPlayer> GetAll()
+        {
+            return new List<FootballPlayer>(Data);
+        }
+
+        public FootballPlayer GetById(int id)
+        {
+            return Data.Find(footballplayer => footballplayer.Id == id);
+        }
+
+        public FootballPlayer Add(FootballPlayer newFootballPlayer)
+        {
+            newFootballPlayer.Id = _nextId++;
+            Data.Add(newFootballPlayer);
+            return newFootballPlayer;
+        }
+
+        public FootballPlayer Delete(int id)
+        {
+            FootballPlayer footballplayer = Data.Find(footballplayer1 => footballplayer1.Id == id);
+            if (footballplayer == null) return null;
+            Data.Remove(footballplayer);
+            return footballplayer;
+        }
+
+        public FootballPlayer Update(int id, FootballPlayer updates)
+        {
+            FootballPlayer footballplayer = Data.Find(footballplayer1 => footballplayer1.Id == id);
+            if (footballplayer == null) return null;
+            footballplayer.Name = updates.Name;
+            footballplayer.ShirtNumber = updates.ShirtNumber;
+            return footballplayer;
+        }
+    }
+}
